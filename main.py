e5c60ecdb13d3e902e31047aec7e4fa185eba545
Josh Elias <josh@daemontech.tools>
2024-04-27 17:00:09 -0400

    add chars count dict

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ def main():
     text = get_book_text(book_path)
     wc = get_word_count(text)
     print("Book Word Count: ", wc)
+    cc = get_chars_dict(text)
+    print("Book Char Counts: ", cc)
 
 def get_word_count(text):
     return len(text.split())
@@ -10,5 +12,14 @@ def get_word_count(text):
 def get_book_text(path): 
     with open(path) as f:
         return f.read()
+
+def get_chars_dict(text):
+    word_counts = {}
+    for c in text:
+        c = c.lower()
+        if not c in word_counts:
+            word_counts[c] = 0
+        word_counts[c] += 1
+    return word_counts
 
 main()
