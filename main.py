00031442628bd64c56a71f9039f40f82e89eb234
Josh Elias <josh@daemontech.tools>
2024-04-27 18:14:43 -0400

    add print_char_report

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 def main():
     book_path = 'books/frankenstein.txt'
     text = get_book_text(book_path)
-    wc = get_word_count(text)
-    print("Book Word Count: ", wc)
+    #wc = get_word_count(text)
     cc = get_chars_dict(text)
-    print("Book Char Counts: ", cc)
+    sorted_char_data = get_char_counts_sorted(cc)
+    print_char_report(sorted_char_data)
+
 
 def get_word_count(text):
     return len(text.split())
@@ -16,10 +17,29 @@ def get_book_text(path):
 def get_chars_dict(text):
     word_counts = {}
     for c in text:
+        if not c.isalpha():
+            continue
         c = c.lower()
         if not c in word_counts:
             word_counts[c] = 0
         word_counts[c] += 1
     return word_counts
+
+def sort_on(dict):
+    return dict['count']
+
+def get_char_counts_sorted(char_dict):
+    char_objects = []
+    for k in char_dict:
+        char_obj = {}
+        char_obj['name'] = k
+        char_obj['count'] = char_dict[k]
+        char_objects.append(char_obj)
+    char_objects.sort(reverse=True, key=sort_on)
+    return char_objects
+
+def print_char_report(char_data_sorted):
+    for v in char_data_sorted:
+        print(f"The '{v["name"]}' character was found {v["count"]} times")
 
 main()
